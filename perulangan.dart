88bd0adc941f694e25ee8d490cab556d0e7d9763
moxz89 <tino.utomo@gmail.com>
2021-10-03 23:14:40 +0700

    menambahkan penggunaan while

--- perulangan.dart
+++ perulangan.dart
@@ -1,7 +1,13 @@
 import 'dart:io';
 
 main(List<String> arguments) {
-  for (int counter = 0; counter < 5; counter = counter + 1) {
-    print("Halo");
+  // for (int counter = 0; counter < 5; counter = counter + 1) {
+  //   print("Halo");
+  // }
+
+  int a = 0;
+  while (a < 5) {
+    print("halo ke " + a.toString());
+    a += 1;
   }
 }

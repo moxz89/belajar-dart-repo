41c78ad6e7d76b9360c4b18ff0bb5d66d072e147
moxz89 <tino.utomo@gmail.com>
2021-10-11 22:42:59 +0700

    menyingkat fungsi tanpa menampung nilai

--- fungsi.dart
+++ fungsi.dart
@@ -2,8 +2,9 @@ import 'dart:io';
 
 double luas_segiempat(double panjang, double lebar) {
   double hasil;
-  hasil = panjang * lebar;
-  return hasil;
+  // hasil = panjang * lebar;
+  // return hasil;
+  return panjang * lebar;
 }
 
 void hello_world() {
@@ -13,12 +14,13 @@ void hello_world() {
 main(List<String> args) {
   hello_world();
 
-  // double p, l, luas;
+  double p, l, luas;
 
-  // p = double.tryParse(stdin.readLineSync());
-  // l = double.tryParse(stdin.readLineSync());
+  p = double.tryParse(stdin.readLineSync());
+  l = double.tryParse(stdin.readLineSync());
 
   // luas = luas_segiempat(p, l);
 
   // print(luas);
+  print(luas_segiempat(p, l));
 }

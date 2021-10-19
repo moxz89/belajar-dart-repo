d3c3fb1021cc6155dd9fe653ff5a20d664aaf055
moxz89 <tino.utomo@gmail.com>
2021-10-19 11:14:28 +0700

    menambahkan for dan foreach

--- list.dart
+++ list.dart
@@ -3,7 +3,17 @@ import 'dart:io';
 main(List<String> args) {
   List<int> mylist = [];
   List<int> list = [1, 2, 3];
-  list[0] = 5;
-  int p = list[0];
-  print(p);
+  // list[0] = 5;
+  // int p = list[0];
+  // print(p);
+
+  // for (int index = 0; index < list.length; index += 1) {
+  //   print(list[index]);
+  // }
+  // for (int bilangan in list) {
+  //   print(bilangan);
+  // }
+  list.forEach((bilangan) {
+    print(bilangan);
+  });
 }
